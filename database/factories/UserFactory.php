65ec7b7ba6d1a4b26c5c132f7006bf950c731de9
vodnicearleonid <vodnicear@gmail.com>
2020-11-23 22:17:42 +0200

    Часть 1. Изучение Laravel Seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace Database\Factories;
-
 use App\Models\User;
+use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+
 
 class UserFactory extends Factory
 {
