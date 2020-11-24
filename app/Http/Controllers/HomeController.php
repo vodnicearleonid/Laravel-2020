6a76e31eb0b8fcec8bfd4e5fc6e70a8a4d3526f1
vodnicearleonid <vodnicear@gmail.com>
2020-11-24 02:09:23 +0200

    Database: Seeding !!!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
 {
     public function index(Request $request){
 
-        $posts = Post::orderBy('id', 'desc')->paginate(3);
+        $posts = Post::orderBy('id', 'desc')->paginate(6);
         $title ='Home page';
         return view('home', compact('title', 'posts'));
     }
